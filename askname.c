52e08c5592bec2a7cd9d1642083d12be12b6ca3a
Ozerov Pavel <kirgano2001@gmail.com>
2023-10-21 23:20:53 +0300

    vse ok
    # Conflicts:
    #	askname.c

--- askname.c
+++ askname.c
@@ -2,7 +2,11 @@
 #include <string.h>
 void askname(char *first, char *last)
 {
+<<<<<<< HEAD
 printf("Enter your first name: CHANGE 4 COPY");
+=======
+printf("Enter your first name: ");
+>>>>>>> d3d1bfb (vse ok)
 fgets(first, 255, stdin);
 first[strlen(first)-1] = '\0'; /* remove the newline at the end */
 printf("Now enter your last name: ");
