3ed7f81a5afb0b762f98e4b285a5f588348bf4d2
Ozerov Pavel <kirgano2001@gmail.com>
2023-10-21 23:06:39 +0300

    Zapyataya

--- tochka c file.c
+++ tochka c file.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-Методические указания к лабораторной работе №2 17
-по курсу «Технологии разработки программного обеспечения»
 #include <string.h>
 int main(int argc, char **argv)
 {
-char name[255];
-printf("Enter your name: ");
-fgets(name, 255, stdin);
-name[strlen(name)-1] = '\0'; /* remove the newline at the end */
-printf("Hello %s!\n", name);
-return 0;
-}
+char first[255], last[255];
+printf("Enter your first name: ");
+fgets(first, 255, stdin);
+first[strlen(first)-1] = '\0'; /* remove the newline at the end */
+printf("Now enter your last name: ");
+gets(last); /* buffer overflow? what's that? */
+printf("Hello %s %s!\n", first, last);
