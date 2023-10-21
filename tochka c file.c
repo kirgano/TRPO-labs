0cc384b862dd63fc2fd435a5c140b2d792950d6a
Ozerov Pavel <kirgano2001@gmail.com>
2023-10-21 23:09:02 +0300

    use proper grammar

--- tochka c file.c
+++ tochka c file.c
@@ -7,7 +7,7 @@ int main(int argc, char **argv)
 char name[255];
 printf("Enter your name: ");
 fgets(name, 255, stdin);
-name[strlen(name)-1] = '\0'; /* remove the newline at the end */
+name[strlen(name)-1] = '\0'; /* remove the newline at the end aaaaaaaaaaaaaaaaaaaaaaaaaaa*/
 printf("Hello %s!\n", name);
 return 0;
 }
